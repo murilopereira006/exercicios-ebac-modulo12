578248c4a5976990fbb69403abbb0b7e786d71a6
muriloPereira006 <muriloedu006@gmail.com>
2023-10-19 00:48:00 -0300

    Exercício Módulo 12

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -1,4 +1,10 @@
 /// <reference types="cypress" />
+const faker = require('faker')
+let dadosLogin
+
+const name = faker.name.firstName()
+const lastName = faker.name.firstName()
+const email = faker.internet.email(name)
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
     /*  Como cliente 
@@ -9,13 +15,48 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         Preenchendo todas opções no checkout
         E validando minha compra ao final */
 
+    before(() => {
+        cy.fixture('perfil').then(perfil => {
+            dadosLogin = perfil
+        })
+    });
+
     beforeEach(() => {
         cy.visit('/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.get('.icon-user-unfollow').click()
+        cy.login(dadosLogin.usuario, dadosLogin.senha)
+        cy.get('.page-title').should('contain', 'Minha conta')
+
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.adicionarProdutoNoCarrinho('Abominable Hoodie', 'L', 'Green', 1)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.adicionarProdutoNoCarrinho('Aether Gym Pant', '34', 'Green', 3)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.adicionarProdutoNoCarrinho('Argus All-Weather Tank', 'M', 'Gray', 1)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+        cy.adicionarProdutoNoCarrinho('Ariel Roll Sleeve Sweatshirt', 'XS', 'Red', 1)
+
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click()
+
+        cy.completarDados(
+            name,
+            lastName,
+            'EBAC',
+            'Brasil',
+            '960 Bruen Lake',
+            'Rio de Janeiro',
+            'Rio de Janeiro',
+            '00000111',
+            '11999994444',
+            email
+        )
+
+        cy.finalizarCompra('Pagamento na entrega')
+
+        cy.get('.page-title').should('contain', 'Pedido recebido')
     });
-
-
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -31,3 +31,32 @@ Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('.woocommerce-form > .button').click()
 });
 
+Cypress.Commands.add('adicionarProdutoNoCarrinho', (nomeDoProduto, tamanho, cor, quantidade) => {
+    cy.get('[class="product-block grid"]').contains(nomeDoProduto).click()
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get('.button-variable-item-' + cor).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+});
+
+Cypress.Commands.add('completarDados', (nome, sobrenome, nomeDaEmpresa, pais, nomeDaRua, cidade, estado, cep, telefone, email) => {
+    cy.get('#billing_first_name').clear().type(nome)
+    cy.get('#billing_last_name').clear().type(sobrenome)
+    cy.get('#billing_company').clear().type(nomeDaEmpresa)
+    cy.get('#select2-billing_country-container').type(pais+'{enter}')
+    cy.get('#billing_address_1').clear().type(nomeDaRua)
+    cy.get('#billing_city').clear().type(cidade)
+    cy.get('#select2-billing_state-container').type(estado+'{enter}')
+    cy.get('#billing_postcode').clear().type(cep)
+    cy.get('#billing_phone').clear().type(telefone)
+    cy.get('#billing_email').clear().type(email)
+});
+
+Cypress.Commands.add('finalizarCompra', (metodoDePagamento) => {
+    if(metodoDePagamento === 'Transferência bancária') cy.get('#payment_method_bacs').click()
+    if(metodoDePagamento === 'Cheque') cy.get('#payment_method_cheque').click()
+    if(metodoDePagamento === 'Pagamento na entrega') cy.get('#payment_method_cod').click()
+    cy.get('#terms').click()
+    cy.get('#place_order').click()
+});
+
